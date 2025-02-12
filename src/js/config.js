cf65c26372e745adee33ba98a45b23d032320f4a
Pedro <plambarri@gmail.com>
2025-02-12 11:29:30 +0100

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.jonas.io/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "3049fff4-0a41-40d0-af55-4b762462fa41";
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/searchView.js
@@ -0,0 +1,24 @@
+import View from "./View.js";
+
+class SearchView {
+  _parentEl = document.querySelector(".search");
+
+  getQuery() {
+    const query = this._parentEl.querySelector(".search__field").value;
+    this._claerInput();
+    return query;
+  }
+
+  _claerInput() {
+    this._parentEl.querySelector(".search__field").value = "";
+  }
+
+  addHandlerSearch(handler) {
+    this._parentEl.addEventListener("submit", function (e) {
+      e.preventDefault();
+      handler();
+    });
+  }
+}
+
+export default new SearchView();
